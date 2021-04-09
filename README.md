094d9b76a93d2903b6e5c06fabf82ee24db43dd7
Chase Clark <chase_clark07@hotmail.com>
2021-04-09 16:10:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# COSC485_Project1_cjclark0
+ Deterministic Finite Automata Simulator made in Java
---
+++ src/main/java/Project1/DFA.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project1;
+
+/**
+ *
+ * @author chase.clark
+ */
+public class DFA {
+    
+    // this method is the first thing called when the program is ran.
+    public void start(){
+        System.out.println("Chase Clark DFA program");
+        System.out.println("Starting program...");
+        System.out.println();
+    }
+}
